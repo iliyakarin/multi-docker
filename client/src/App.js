4c3bef45e43b6009736ed97af933490ef05ead9a
root <root@centos1-k8s.homelab>
2019-06-14 16:58:59 +0300

    changed name

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,9 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fibonacci calculator</h1>
             <Link to="/">Home</Link>
+            /n
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
