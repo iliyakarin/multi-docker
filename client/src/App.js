04c7cd9496871d9e27de6c92300196d0a44fc2dc
root <root@centos-compose.local>
2021-02-01 17:08:07 +0300

    Fixed link text 2

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home Page</Link>
+          <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
